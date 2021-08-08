cf5f1fe00b009b881aa8ac05bd4abfce0177c33c
hodafarhoomand <farhoomand63@gmail.com>
2021-08-08 13:47:20 +0430

    create model classes

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=200 )
+
+    def __str__(self) :
+        return self.name
+
+        class Book(models.Model):
+            title = models.CharField(max_length = 200)
+            athour = models.ForeignKey('Author', on_delete=models.SET_NULL ,null=True)
+            summary = models.TextField(max_length = 1000 )
+            isbn = models.CharField('ISBN' , max_length = 13)
+            genre =models.ManyToManyField(Genre)
+
+            def __str__(self):
+                return self.title
+                def get_absolute_url():
+                    return reverse('book-detail' , args=[str(self.id)])
+class 
